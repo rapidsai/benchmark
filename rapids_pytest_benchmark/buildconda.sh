400c65d96de30350332b20ec7660e61cc29ffb0e
Rick Ratzel <rratzel@nvidia.com>
2020-05-11 16:40:27 -0500

    Made packages python version-specific to workaround pypy-related issue here: https://github.com/conda-forge/conda-forge.github.io/issues/867#issuecomment-594985046

--- rapids_pytest_benchmark/buildconda.sh
+++ rapids_pytest_benchmark/buildconda.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
 
 set -e
-CHANNELS="-c rlratzel -c conda-forge"
+CHANNELS="-c file:///opt/conda/envs/rapids/conda-bld -c rlratzel -c conda-forge"
 
 UPLOAD_FILE=`conda build ${CHANNELS} ./conda --output`
-conda build ${CHANNELS} ./conda
+UPLOAD_FILES=$(echo ${UPLOAD_FILE}|sed -e 's/\-py[0-9][0-9]/\-py36/')
+UPLOAD_FILES="${UPLOAD_FILES} $(echo ${UPLOAD_FILE}|sed -e 's/\-py[0-9][0-9]/\-py37/')"
+
+conda build ${CHANNELS} --variants="{python: [3.6, 3.7]}" ./conda
 if [ "$1" = "--publish" ]; then
-    anaconda upload ${UPLOAD_FILE}
+    anaconda upload ${UPLOAD_FILES}
 fi

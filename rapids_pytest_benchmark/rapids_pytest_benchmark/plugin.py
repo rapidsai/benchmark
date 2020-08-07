f3d4db34bdf51c220f9f345efdee9479161aa971
dillon-cullinan <dcullinan92@gmail.com>
2020-08-07 07:37:11 -0700

    Use addResults instead of addResult

--- rapids_pytest_benchmark/rapids_pytest_benchmark/plugin.py
+++ rapids_pytest_benchmark/rapids_pytest_benchmark/plugin.py
@@ -531,7 +531,8 @@ def pytest_sessionfinish(session, exitstatus):
             bench.stats.mean
             getattr(bench.stats, "gpu_mem", None)
             getattr(bench.stats, "gpu_util", None)
-
+            
+            resultList = []
             for statType in ["mean", "gpu_mem", "gpu_util"]:
                 bn = "%s_%s" % (benchName, suffixDict[statType])
                 val = getattr(bench.stats, statType, None)
@@ -540,7 +541,9 @@ def pytest_sessionfinish(session, exitstatus):
                                               argNameValuePairs=list(params.items()),
                                               result=val)
                     bResult.unit = unitsDict[statType]
-                    db.addResult(bInfo, bResult)
+                    resultList.append(bResult)
+            
+            db.addResults(bInfo, resultList)
 
 
 def pytest_report_header(config):

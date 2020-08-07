e04b8b28663357c69d90443e80352d3d159988bd
Dillon Cullinan <dcullinan@nvidia.com>
2020-08-07 15:13:54 -0400

    Revert "[REVIEW] Use addResults instead of addResult"

--- rapids_pytest_benchmark/rapids_pytest_benchmark/plugin.py
+++ rapids_pytest_benchmark/rapids_pytest_benchmark/plugin.py
@@ -430,6 +430,9 @@ def _getHierBenchNameFromFullname(benchFullname):
 
 
 def pytest_sessionfinish(session, exitstatus):
+    if exitstatus != 0:
+        return
+
     gpuBenchSess = session.config._gpubenchmarksession
     config = session.config
     asvOutputDir = config.getoption("benchmark_asv_output_dir")
@@ -528,8 +531,7 @@ def pytest_sessionfinish(session, exitstatus):
             bench.stats.mean
             getattr(bench.stats, "gpu_mem", None)
             getattr(bench.stats, "gpu_util", None)
-            
-            resultList = []
+
             for statType in ["mean", "gpu_mem", "gpu_util"]:
                 bn = "%s_%s" % (benchName, suffixDict[statType])
                 val = getattr(bench.stats, statType, None)
@@ -538,9 +540,7 @@ def pytest_sessionfinish(session, exitstatus):
                                               argNameValuePairs=list(params.items()),
                                               result=val)
                     bResult.unit = unitsDict[statType]
-                    resultList.append(bResult)
-            
-            db.addResults(bInfo, resultList)
+                    db.addResult(bInfo, bResult)
 
 
 def pytest_report_header(config):

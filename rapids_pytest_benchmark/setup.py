40ae7b2b4967b900a3514a5d462c35612cef546d
AJ Schmidt <aschmidt@nvidia.com>
2021-03-04 17:59:23 -0500

    add rmm dep

--- rapids_pytest_benchmark/setup.py
+++ rapids_pytest_benchmark/setup.py
@@ -6,7 +6,7 @@ setup(
         name="rapids-pytest-benchmark",
         version=rapids_pytest_benchmark.__version__,
         packages=["rapids_pytest_benchmark"],
-        install_requires=["pytest-benchmark", "asvdb", "pynvml", "pygal"],
+        install_requires=["pytest-benchmark", "asvdb", "pynvml", "pygal", "rmm"],
         # the following makes a plugin available to pytest
         entry_points={"pytest11": ["rapids_benchmark = rapids_pytest_benchmark.plugin"]},
         # custom PyPI classifier for pytest plugins

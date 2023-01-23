da738545d76964276c92291c06efd3db68f97c66
Rick Ratzel <rratzel@nvidia.com>
2023-01-23 00:14:40 -0600

    Removes pygal dependency from setup.py since it is optional and handled by pytest-benchmark

--- rapids_pytest_benchmark/setup.py
+++ rapids_pytest_benchmark/setup.py
@@ -6,7 +6,7 @@ setup(
         name="rapids-pytest-benchmark",
         version=rapids_pytest_benchmark.__version__,
         packages=["rapids_pytest_benchmark"],
-        install_requires=["pytest-benchmark", "asvdb", "pynvml", "pygal", "rmm"],
+        install_requires=["pytest-benchmark", "asvdb", "pynvml", "rmm"],
         # the following makes a plugin available to pytest
         entry_points={"pytest11": ["rapids_benchmark = rapids_pytest_benchmark.plugin"]},
         # custom PyPI classifier for pytest plugins

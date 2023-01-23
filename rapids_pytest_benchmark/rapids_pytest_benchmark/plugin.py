bfd9c3298e87469f353d4e0daa166935f9cc3bb0
Rick Ratzel <rratzel@nvidia.com>
2023-01-22 23:53:35 -0600

    bugfix when updating ASV db files using non-GPU stats, moves import so pygal could be optional, allows for newer pytest-benchmark dependencies in conda meta.yaml, adds copyright blocks, bumps version

--- rapids_pytest_benchmark/rapids_pytest_benchmark/plugin.py
+++ rapids_pytest_benchmark/rapids_pytest_benchmark/plugin.py
@@ -1,3 +1,16 @@
+# Copyright (c) 2020-2023, NVIDIA CORPORATION.
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
 from functools import partial
 import time
 import platform
@@ -602,14 +615,15 @@ def pytest_sessionfinish(session, exitstatus):
 
             # If there were any custom metrics, add each of those as well as an
             # individual result to the same bInfo isntance.
-            for customMetricName in bench.stats.getCustomMetricNames():
-                (result, unitString) = bench.stats.getCustomMetric(customMetricName)
-                bn = "%s_%s" % (benchName, customMetricName)
-                bResult = BenchmarkResult(funcName=bn,
-                                          argNameValuePairs=list(params.items()),
-                                          result=result)
-                bResult.unit = unitString
-                resultList.append(bResult)
+            if isinstance(bench.stats, GPUStats):
+                for customMetricName in bench.stats.getCustomMetricNames():
+                    (result, unitString) = bench.stats.getCustomMetric(customMetricName)
+                    bn = "%s_%s" % (benchName, customMetricName)
+                    bResult = BenchmarkResult(funcName=bn,
+                                              argNameValuePairs=list(params.items()),
+                                              result=result)
+                    bResult.unit = unitString
+                    resultList.append(bResult)
 
             db.addResults(bInfo, resultList)
 

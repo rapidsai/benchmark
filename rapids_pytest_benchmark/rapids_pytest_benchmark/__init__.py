d4ecae0cbf2500062c23b05a1aabfdd212f92caa
Rick Ratzel <rratzel@nvidia.com>
2023-05-12 11:08:30 -0500

    Bumps version.

--- rapids_pytest_benchmark/rapids_pytest_benchmark/__init__.py
+++ rapids_pytest_benchmark/rapids_pytest_benchmark/__init__.py
@@ -11,7 +11,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-__version__ = "0.0.15"
+__version__ = "0.0.16"
 
 def setFixtureParamNames(request, orderedParamNameList):
     """
